57804f4293c360447cb9635128ae8525ff294955
Niina Siitari <siitari.niina@gmail.com>
2021-09-07 13:59:39 +0300

    Tehtiin GetPostsByCriteria controller ja view sekä linkki navbariin.

---
+++ Loppuprojekti_AW/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using Loppuprojekti_AW.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loppuprojekti_AW.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly MoveoContext _context;
+        public SearchController(MoveoContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetPostsByCriteria(string criteria)
+        {
+            if (criteria != null)
+            {
+                DataAccess DA = new DataAccess(_context);
+                var postlist = DA.GetPostsByCriteria(criteria);
+                ViewBag.Posts = postlist;
+                return View();
+            } else {
+                //string error = "Anna hakusana"
+                //ViewBag.Alert = error; Tässä viewbagiin menee error viesti joka tulostetaan viewissä bootstrap alertissa
+                return View();
+            }
+        }
+    }
+}
