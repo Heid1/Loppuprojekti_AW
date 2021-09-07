a048314b651e11c50b57561b9cae3ee95373dc0a
JanicaJ <janica.jokela@awacademyconsultant.fi>
2021-09-07 15:00:31 +0300

    hätävarasession luotu, vaatii vain usernamen formista

--- Loppuprojekti_AW/Controllers/HomeController.cs
+++ Loppuprojekti_AW/Controllers/HomeController.cs
@@ -6,21 +6,39 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Loppuprojekti_AW.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MoveoContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MoveoContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Name) // await missing?
+        {
+            var user = _context.Endusers.Where(u => u.Username == Name).FirstOrDefault();
+
+            if (user != null)
+            {
+                HttpContext.Session.SetString("username", user.Username);
+                return View();
+            }
+            ModelState.AddModelError("Name", "There is no account assosiated with the given name. Please try again or create a new account!");
+            return View();
+
         }
 
         public IActionResult Privacy()
