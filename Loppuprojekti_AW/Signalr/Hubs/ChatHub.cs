1a4849e20e7331c4b15344d9c63c41122d7041fa
Heidi Johanna <vahataloh@gmail.com>
2021-09-09 23:35:43 +0300

    chat hub muokkauksia

--- Loppuprojekti_AW/Signalr/Hubs/ChatHub.cs
+++ Loppuprojekti_AW/Signalr/Hubs/ChatHub.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.SignalR;
 
 
 using System.Web;
+using Microsoft.AspNetCore.Http;
 
 namespace Loppuprojekti_AW.Signalr.Hubs
 {
@@ -16,6 +17,43 @@ namespace Loppuprojekti_AW.Signalr.Hubs
         {
             // Call the addNewMessageToPage method to update clients.
             await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override Task OnConnected()
+        {
+            // Get UserID. Assumed the user is logged before connecting to chat and userid is saved in session.
+            string UserID = (string)HttpContext.Current.Session["userid"];
+          
+            // Get ChatHistory and call the client function. See below
+            this.GetHistory(UserID);
+
+            // Get ConnID
+            string ConnID = Context.ConnectionId;
+
+            // Save them in DB. You got to create a DB class to handle this. (Optional)
+            DB.UpdateConnID(UserID, ConnID);
+        }
+
+        private void GetHistory(UserID)
+        {
+            // Get Chat History from DB. You got to create a DB class to handle this.
+            string History = DB.GetChatHistory(UserID);
+
+            // Send Chat History to Client. You got to create chatHistory handler in Client side.
+            Clients.Caller.chatHistory(History);
+        }
+
+        // This method is to be called by Client 
+        public void Chat(string Message)
+        {
+            // Get UserID. Assumed the user is logged before connecting to chat and userid is saved in session.
+            string UserID = (string)HttpContext.Current.Session["userid"];
+
+            // Save Chat in DB. You got to create a DB class to handle this
+            DB.SaveChat(UserID, Message);
+
+            // Send Chat Message to All connected Clients. You got to create chatMessage handler in Client side.
+            Clients.All.chatMessage(Message);
         }
     }
 }
