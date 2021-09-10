f464b5975574e96315de4606b992748ec44a315c
JanicaJ <janica.jokela@awacademyconsultant.fi>
2021-09-10 16:26:07 +0300

    ...

--- Loppuprojekti_AW/Controllers/LoginController.cs
+++ Loppuprojekti_AW/Controllers/LoginController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Loppuprojekti_AW.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
 
 namespace Loppuprojekti_AW.Controllers
 {
@@ -23,15 +25,25 @@ namespace Loppuprojekti_AW.Controllers
         [HttpPost]
         public IActionResult Index(string Email, string Password)
         {
-            Enduser Eu = _context.Endusers.Where(u => u.Email == Email && u.Password == Password).FirstOrDefault();
+            //Enduser Eu = _context.Endusers.Where(u => u.Email == Email && u.Password == Password).FirstOrDefault();
+            Enduser enduser = _context.Endusers.Where(u => u.Email == Email).FirstOrDefault();
 
-            if (Eu != null)
+            if (enduser != null)
             {
-                HttpContext.Session.SetInt32("userid", Eu.Userid);
-                HttpContext.Session.SetString("username", Eu.Username);
-                HttpContext.Session.SetString("email", Eu.Email);
-                HttpContext.Session.SetString("userrole", Eu.Userrole);
-                return RedirectToAction("Index", "Account");
+                if (Validate(Password, enduser.Password))
+                {
+                    HttpContext.Session.SetInt32("userid", enduser.Userid);
+                    HttpContext.Session.SetString("username", enduser.Username);
+                    HttpContext.Session.SetString("email", enduser.Email);
+                    HttpContext.Session.SetString("userrole", enduser.Userrole);
+                    return RedirectToAction("Index", "Account");
+                }
+                else
+                {
+                    ModelState.AddModelError("Password", "You entered a wrong password! Please try again.");
+                    ViewBag.AuthOK = false;
+                    return View();
+                }
             }
             else
             {
@@ -40,5 +52,19 @@ namespace Loppuprojekti_AW.Controllers
                 return View();
             }
         }
+        public static string Hash(string value)
+        {
+            var valueBytes = KeyDerivation.Pbkdf2(
+                                password: value,
+                                salt: Encoding.UTF8.GetBytes("Passw0rd"),
+                                prf: KeyDerivationPrf.HMACSHA512,
+                                iterationCount: 10000,
+                                numBytesRequested: 256 / 8);
+
+            return Convert.ToBase64String(valueBytes);
+        }
+
+        public static bool Validate(string value, string hash)
+            => Hash(value) == hash;
     }
 }
