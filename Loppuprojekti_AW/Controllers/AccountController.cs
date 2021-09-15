b11058db1a2399c31d67359d3a9d7cd9556b46f6
JanicaJ <janica.jokela@awacademyconsultant.fi>
2021-09-15 11:11:41 +0300

    profiilikuvan korjaus, indeksöinti

--- Loppuprojekti_AW/Controllers/AccountController.cs
+++ Loppuprojekti_AW/Controllers/AccountController.cs
@@ -34,7 +34,7 @@ namespace Loppuprojekti_AW.Controllers
             blobServiceEndpoint = configuration["StorageEndPoint"];
         }
 
-        public async Task <IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             DataAccess da = new DataAccess(_context);
             var userid = HttpContext.Session.GetInt32("userid");
@@ -98,7 +98,7 @@ namespace Loppuprojekti_AW.Controllers
         {
             if (Photo == null)
             {
-                return null;
+                return photoUrl;
             }
             using Stream imageStream = Photo.OpenReadStream();
             StorageSharedKeyCredential accountCredentials = new StorageSharedKeyCredential(storageAccountName, storageAccountKey);
@@ -116,7 +116,7 @@ namespace Loppuprojekti_AW.Controllers
             blob = container.GetBlobClient(photoname);
             //if (blob.Exists())
             //{
-                return blob.Uri.ToString();
+            return blob.Uri.ToString();
             //}
             //container = service.GetBlobContainerClient(photoBlob);
             //blob = container.GetBlobClient(photoname);
